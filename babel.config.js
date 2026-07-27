580976708637ba1ac1f512e2a4036106199cfa9d
yunemak <yeak558@gmail.com>
2026-07-27 09:46:02 +0300

    feat: Use babel as instructed

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,4 @@
-module.exports = {
-	presets: [["@babel/preset-env", { targets: { node: "current" } }]],
+export default {
+  presets: [["@babel/preset-env", { targets: { node: "current" } }]],
 };
+
