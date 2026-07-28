e07e1f1fcd02dd7873ea10aee7a73329382a81f7
yunemak <yeak558@gmail.com>
2026-07-28 11:50:30 +0300

    Add more test for caesarCipher and fail

--- caesarCipher/caesarCipher.test.js
+++ caesarCipher/caesarCipher.test.js
@@ -3,3 +3,15 @@ import { caesarCipher } from "./caesarCipher";
 test("Basic Case", () => {
 	expect(caesarCipher("abc", 0)).toBe("abc");
 });
+
+test("Basic Case", () => {
+	expect(caesarCipher("abc", 1)).toBe("bcd");
+});
+
+test("Basic Case", () => {
+	expect(caesarCipher("abc", 2)).toBe("cde");
+});
+
+test("Basic Case", () => {
+	expect(caesarCipher("abc", 3)).toBe("def");
+});
