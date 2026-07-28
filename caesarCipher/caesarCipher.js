7cd1b94ab0cebf8c2e3b7af200b09399bd186dae
yunemak <yeak558@gmail.com>
2026-07-28 12:03:42 +0300

    Pass basic tests

--- caesarCipher/caesarCipher.js
+++ caesarCipher/caesarCipher.js
@@ -1,5 +1,9 @@
 function caesarCipher(str, num) {
-	return str;
+	let array = [];
+	for (let c = 0; c < str.length; c++) {
+		array.push(String.fromCharCode(str[c].charCodeAt(0) + num));
+	}
+	return array.join("");
 }
 
 export { caesarCipher };
